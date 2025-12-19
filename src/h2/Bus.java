5df668f168949c0af33de7df1ff9cf78f2c2b046
regular <regular@regular.local>
2025-12-19 23:59:40 +0100

    <MESSAGE>

--- src/h2/Bus.java
+++ src/h2/Bus.java
@@ -43,13 +43,13 @@ public class Bus {
         return passengersWithoutTickets;
     }
     public void transferPassengers(Bus otherBus, String[] passengerNames) {
-        int otherBusSize = otherBus.passengers.size();
-        for (int i = 0; i < passengerNames.length; i++) {
-            for (int j = passengers.size() - 1; j >= 0; j--) {
-                Passenger p = passengers.get(j);
-                if (p.name == passengerNames[i]) {
-                    otherBus.passengers.add(otherBusSize, p);
+        for (int j = passengers.size() - 1; j >= 0; j--) {
+            Passenger p = passengers.get(j);
+            for (int i = 0; i < passengerNames.length; i++) {
+                if (p.name.equals(passengerNames[i])) {
+                    otherBus.passengers.add(p);
                     passengers.remove(j);
+                    break;
                 }
             }
         }
