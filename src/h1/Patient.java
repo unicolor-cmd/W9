a0017bd83214e3fcbd22b1bacae8e88fc1269e43
regular <regular@regular.local>
2025-12-20 00:22:55 +0100

    <MESSAGE>

--- src/h1/Patient.java
+++ src/h1/Patient.java
@@ -3,7 +3,6 @@ package h1;
 public class Patient {
     public String name;
     public int prio;
-
     public Patient(String name, int prio) {
         this.name = name;
         this.prio = prio;
